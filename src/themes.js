3921f0c9f8979decdf5d9f86057c4183a6bb2a87
Diego Jacomel <dmeloj@gmail.com>
2020-02-17 17:11:18 -0300

    test commit

--- src/themes.js
+++ src/themes.js
@@ -104,6 +104,7 @@ const boticarioTheme = {
         light: 100,
         normal: 400,
         bold: 700,
+        x: 0,
     },
     fontFamily: {
         default: `"Lato", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"`,
